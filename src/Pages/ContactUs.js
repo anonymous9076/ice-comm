2aefc53f6c055e00441e157b0eef27d47d2209e3
Tushar kumar <68018920+anonymous9076@users.noreply.github.com>
2024-03-21 12:19:51 +0530

    Update ContactUs.js

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,10 +1,39 @@
-function ContactUs(){
-    return(
+import React from 'react'
+import './Contact.css'
 
-        <div className="contactpage neon" style={{height:'100vh',textAlign:'center',padding:'15%'}}>
-        <h1>Support system</h1>
-        <p>Due to shortage of support team contact services is currently out of service, To contact me msg me on insta</p>
+function Contact() {
+  return (
+    <div className='sec5'>
+        <h1 className='colorful'> Contact Me :</h1>
+        <div className='contact-box'>
+           
+                <form  className='cont-area' action='https://formspree.io/f/xnqeqnad' method='post'>
+                <span className='enter-field'>
+                    <label>First Name</label><br></br>
+                    <input name='firstname' placeholder='Enter your firstname' ></input>
+                </span>
+                <span className='enter-field'>
+                    <label>Last Name</label><br></br>
+                    <input name='lastname' placeholder='Enter your lastname'></input>
+                </span>
+                <span className='enter-field'>
+                    <label>E-mail</label><br></br>
+                    <input name='email' placeholder='Enter your email'></input>
+                </span>
+                <span className='enter-field'>
+                    <label>Phone-No.</label><br></br>
+                    <input name='phn-no.' placeholder='Enter your number'></input>
+                </span>
+                <span className='enter-field'>
+                    <label>Your Query</label><br></br>
+                    <textarea name='message' placeholder='Enter your query'></textarea>
+                </span>
+                <span className='enter-field for-btn'><button type='submit' className='sb-btn'>Submit</button></span>
+                </form>
+            
         </div>
-    )
+    </div>
+  )
 }
-export default ContactUs;
+
+export default Contact
